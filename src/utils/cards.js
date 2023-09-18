eb04d514f73c8836e35d0cb4731bef84751c92cd
Dylan <dylanquach@gmail.com>
2023-09-18 09:12:16 -0700

    Fix card images

--- src/utils/cards.js
+++ src/utils/cards.js
@@ -1,27 +1,3 @@
-function renderDeck(deck) {
-    const deckElement = document.getElementById('deck');
-    deckElement.innerHTML = '';
-    for (const card of deck) {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.innerText = card;
-        deckElement.appendChild(cardElement);
-    }
-}
-
-function shuffleDeck() {
-    const deck = createDeck();
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-    renderDeck(deck);
-}
-
-// Initial deck rendering
-const initialDeck = createDeck();
-renderDeck(initialDeck);
-
 // const cardObject = [
 //     {id:1, imagePath:'../images/c, suit: h, value 13},
 //     {id:2, imagePath:'../images/', suit: c, value: 10},
