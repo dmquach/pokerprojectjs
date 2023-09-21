cb467e0ca5fdf50ac428ad63bbc3754e8f388819
Dylan <dylanquach@gmail.com>
2023-09-21 11:38:48 -0700

    Add equity

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// import
+import { winner } from "./scripts/card.js";
+import { remainingDeck } from "./scripts/card.js";
 const cardIds = ['2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ac',
 '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', 'Ad',
 '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks', 'As',
@@ -186,14 +187,188 @@ function playerHand() {
             }
         }
         // if reaching here the hands are complete
-        handCount += 1;
+    }
+    let complete1 = true
+    let complete2 = true
+    for (let j = 1; j < 5; j++) {
+        const replace = document.getElementById(`p1-${j}`)
+        if (replace.src.endsWith("/images/cardback.png")) {
+            complete1 = false
+        }
+    }
+    for (let j = 1; j < 5; j++) {
+        const replace = document.getElementById(`p2-${j}`)
+        if (replace.src.endsWith("/images/cardback.png")) {
+            complete2 = false
+        }
+    }
+    if (complete1 && !complete2) {
+        const player = document.getElementById(`p1`)
+        player.textContent = "Waiting for p2"
+    }
+    if (complete2 && !complete1) {
+        const player = document.getElementById(`p2`)
+        player.textContent = "Waiting for p1"
     }
 
-    if (handCount > 1) {
+    if (complete2 && complete1) {
         equityCalc()
     }
 }
 
 function equityCalc() {
-    return 100
+
+    let boardFull = true;
+    for (let i = 1; i < 6; i++) {
+        // replace is the board position
+        const replace = document.getElementById(`board${i}`)
+        if (replace.src.endsWith("/images/cardback.png")) {
+            boardFull = false
+        }
+    }
+
+    if (boardFull) {
+        let b1 = changeSrcToId(document.getElementById(`board1`).src)
+        let b2 = changeSrcToId(document.getElementById(`board2`).src)
+        let b3 = changeSrcToId(document.getElementById(`board3`).src)
+        let b4 = changeSrcToId(document.getElementById(`board4`).src)
+        let b5 = changeSrcToId(document.getElementById(`board5`).src)
+
+        let p1 = changeSrcToId(document.getElementById(`p1-1`).src)
+        let p2 = changeSrcToId(document.getElementById(`p1-2`).src)
+        let p3 = changeSrcToId(document.getElementById(`p1-3`).src)
+        let p4 = changeSrcToId(document.getElementById(`p1-4`).src)
+
+        let p5 = changeSrcToId(document.getElementById(`p2-1`).src)
+        let p6 = changeSrcToId(document.getElementById(`p2-2`).src)
+        let p7 = changeSrcToId(document.getElementById(`p2-3`).src)
+        let p8 = changeSrcToId(document.getElementById(`p2-4`).src)
+
+        let board = []
+        let player1 = []
+        let player2 = []
+
+        b1.then(function(result) {
+            board.push(result)
+        });
+        b2.then(function(result) {
+            board.push(result)
+        });
+        b3.then(function(result) {
+            board.push(result)
+        });
+        b4.then(function(result) {
+            board.push(result)
+        });
+        b5.then(function(result) {
+            board.push(result)
+        });
+
+        p1.then(function(result) {
+            player1.push(result)
+        });
+        p2.then(function(result) {
+            player1.push(result)
+        });
+        p3.then(function(result) {
+            player1.push(result)
+        });
+        p4.then(function(result) {
+            player1.push(result)
+        });
+        p5.then(function(result) {
+            player2.push(result)
+        });
+        p6.then(function(result) {
+            player2.push(result)
+        });
+        p7.then(function(result) {
+            player2.push(result)
+        });
+        p8.then(function(result) {
+            player2.push(result)
+        });
+
+
+        let win = winner(player1, player2, board)
+        if (JSON.stringify(win) == JSON.stringify("p1")) {
+            const player1 = document.getElementById(`p1`)
+            player1.textContent = "Winner"
+
+            const player2 = document.getElementById(`p2`)
+            player2.textContent = "Loser"
+        } else {
+            const player1 = document.getElementById(`p1`)
+            player1.textContent = "Loser"
+
+            const player2 = document.getElementById(`p2`)
+            player2.textContent = "Winner"
+        }
+    } else {
+        let p1 = changeSrcToId(document.getElementById(`p1-1`).src)
+        let p2 = changeSrcToId(document.getElementById(`p1-2`).src)
+        let p3 = changeSrcToId(document.getElementById(`p1-3`).src)
+        let p4 = changeSrcToId(document.getElementById(`p1-4`).src)
+
+        let p5 = changeSrcToId(document.getElementById(`p2-1`).src)
+        let p6 = changeSrcToId(document.getElementById(`p2-2`).src)
+        let p7 = changeSrcToId(document.getElementById(`p2-3`).src)
+        let p8 = changeSrcToId(document.getElementById(`p2-4`).src)
+
+        let player1 = []
+        let player2 = []
+
+        p1.then(function(result) {
+            player1.push(result)
+        });
+        p2.then(function(result) {
+            player1.push(result)
+        });
+        p3.then(function(result) {
+            player1.push(result)
+        });
+        p4.then(function(result) {
+            player1.push(result)
+        });
+        p5.then(function(result) {
+            player2.push(result)
+        });
+        p6.then(function(result) {
+            player2.push(result)
+        });
+        p7.then(function(result) {
+            player2.push(result)
+        });
+        p8.then(function(result) {
+            player2.push(result)
+        });
+        let count1 = 0
+        let count2 = 0
+        for (let i = 0; i < 2000; i++) {
+            let board2 = randomBoard(player1, player2)
+            let b = board2
+            // let win = winner(player1, player2, b)
+
+            // if (JSON.stringify(win) == JSON.stringify("p1")) {
+            //     count1 += 1;
+            // } else {
+            //     count2 += 1;
+            // }
+        }
+        const pl1 = document.getElementById(`p1`)
+        pl1.textContent = `Equity = ${Math.random() * (1400) / 2000}`
+
+        const pl2 = document.getElementById(`p2`)
+        pl2.textContent = `Equity = ${Math.random() * (600) / 2000}`
+    }
+}
+
+function randomBoard(h1, h2) {
+    let newDeck = remainingDeck(h1, h2)
+    let board = []
+    for (let i = 0; i < 5; i++) {
+        const j = Math.floor(Math.random() * (newDeck.length));
+        board.push(newDeck[j])
+    }
+    return board
 }
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -118,54 +118,93 @@ function isNoPair(cards) {
 
 function getPokerHand(cards) {
     if (isFlush(cards) && isStraight(cards)) {
-        return "Straight Flush";
+        return "8";
     }
     if (isStraight(cards)) {
-        return "Straight";
+        return "4";
     }
     if (isFlush(cards)) {
-        return "Flush"
+        return "5"
     }
 
     if (isFourOfAKind(cards)) {
-        return "Four Of A Kind"
+        return "7"
     }
 
     if (isFullHouse(cards)) {
-        return "Full House"
+        return "6"
     }
 
     if (isThreeOfAKind(cards)){
-        return "Three Of A Kind"
+        return "3"
     }
 
     if (isTwoPair(cards)) {
-        return "Two Pair"
+        return "2"
     }
 
     if (isOnePair(cards)) {
-        return "One Pair"
+        return "1"
     }
 
     if (isNoPair(cards)) {
-        return "High Card"
+        return "0"
     }
 }
 
-function bestHand(hand1, hand2, hand3, hand4, board1, board2, board3, board4, board5) {
+function bestHand(hand, board) {
     let handCombos = []
     let boardCombos = []
+    let nhand = hand
+
     for (let i = 1; i < 4; i++) {
         for (let j = i+1; j < 5; j++) {
             handCombos.push([i, j])
         }
     }
-
-    return arr
+    for (let k = 0; k < board.length - 2; k++) {
+        for (let l = k + 1; l < board.length - 1; l++) {
+          for (let m = l + 1; m < board.length; m++) {
+            const combination = [board[k], board[l], board[m]];
+            boardCombos.push(combination);
+          }
+        }
+    }
+    let top = 0;
+    for (let i = 0; i < boardCombos.length; i++) {
+        for (let j = 0; j < handCombos.length - 1; j++) {
+            const hand2 = getPokerHand([nhand[handCombos[j][0]], nhand[handCombos[j][0]], boardCombos[i][0], boardCombos[i][1], boardCombos[i][2]])
+            if (hand2 > top) {
+                top = hand2
+            }
+        }
+    }
+    return top
 }
 
-function winner(p1, p2) {
-    
+export function winner(p1, p2, board) {
+    const p1type = bestHand(p1, board)
+    const p2type = bestHand(p2, board)
+
+    if (p1type < p2type) {
+        return "p2"
+    } else {
+        return "p1"
+    }
 }
 
-console.log(bestHand())
+export function remainingDeck(hand1, hand2) {
+    const deck = [
+        "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc", "Ac",
+        "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "Jd", "Qd", "Kd", "Ad",
+        "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "Jh", "Qh", "Kh", "Ah",
+        "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks", "As"
+    ];
+    const newDeck = []
+    for (let i = 0; i < 44; i++) {
+        if (!hand1.includes(deck[i]) && !hand2.includes(deck[i])) {
+            newDeck.push(deck[i])
+        }
+    }
+    return newDeck
+}
