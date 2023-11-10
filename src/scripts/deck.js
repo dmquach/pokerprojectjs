ef3c2831d977085ec9b794728274c08f9b65a8d0
Dylan <dylanquach@gmail.com>
2023-11-10 12:02:55 -0800

    Create board compatibility deck.js

--- src/scripts/deck.js
+++ src/scripts/deck.js
@@ -6,8 +6,8 @@ function Deck () {
         '2h': 'deck', '3h': 'deck', '4h': 'deck', '5h': 'deck', '6h': 'deck', '7h': 'deck', '8h': 'deck', '9h': 'deck', '10h': 'deck', 'Jh': 'deck', 'Qh': 'deck', 'Kh': 'deck', 'Ah': 'deck'
     }
     this.boardPos = {
-        'p1-1': '', 'p1-2': '', 'p1-3': '', 'p1-4': '',
-        'p2-1': '', 'p2-2': '', 'p2-3': '', 'p2-4': '',
+        'p1-1': 'open', 'p1-2': 'open', 'p1-3': 'open', 'p1-4': 'open',
+        'p2-1': 'open', 'p2-2': 'open', 'p2-3': 'open', 'p2-4': 'open',
         'p3-1': '', 'p3-2': '', 'p3-3': '', 'p3-4': '',
         'p4-1': '', 'p4-2': '', 'p4-3': '', 'p4-4': '',
         'p5-1': '', 'p5-2': '', 'p5-3': '', 'p5-4': '',
@@ -17,7 +17,7 @@ function Deck () {
     }
     this.numPlayers = 2
     this.addClickCards()
-    this.addClickBoard
+    this.addClickBoard()
 }
 
 Deck.prototype.addClickCards = function () {
@@ -37,6 +37,7 @@ Deck.prototype.addClickCards = function () {
 Game.prototype.addClickBoard = function () {
     for (let pos in this.boardPos) {
         const newPos = document.getElementById(pos);
+        // newPos.id = p1-1
         newPos.addEventListener("click", () => {
             if (this.boardPos[newPos.id] === '') {
                 this.boardPos['highlight'] = newPos.id
@@ -51,14 +52,86 @@ Game.prototype.addClickBoard = function () {
 Deck.prototype.addToBoard = function (cardKey) {
     // add card to board
     if (this.boardPos['highlight'] === '') {
-        
+        for (let pos in this.boardPos) {
+            if (this.boardPos[pos] === 'open') {
+                // pos = first open pos p1-1
+                // cardKey = "2c"
+                this.boardPos[pos] = 'taken'
+                this.cardDeck[cardKey] = 'board'
+                //  <img src="./images/cardback.png" id="p1-1">
+                const changePos = document.getElementById(pos);
+                const tempSrc = changePos.src;
+                //  <img src="./images/2_of_hearts.png" id="2h">
+                const changeCard = document.getElementById(key);
+                changePos.src = changeCard.src;
+                changeCard.src = tempSrc;
+                return console.log("switched")
+            }
+        }
+        // if none open
+        return console.log("no open spaces")
     }
 }
 
 Deck.prototype.removeFromBoard = function (boardKey) {
+    // boardKey = p1-1
+    const changePos = document.getElementById(boardKey);
+    const tempSrc = changePos.src;
 
+    this.boardPos[changePos.id] = 'open'
+    // cardKey "2h"
+    const cardKey = this.changeSrcToId(tempSrc)
+    this.cardDeck[cardKey] = 'deck'
+
+    const changeCard = document.getElementById(cardKey);
+    changePos.src = changeCard.src;
+    changeCard.src = tempSrc;
+    return console.log("switched")
 }
 
-Deck.prototype.returnToDeck = function (key) {
+Deck.prototype.changeSrcToId = function (src) {
+    let val;
+    let word;
+    if (src.includes('spades')) {
+        suit = "s"
+        word = src.slice(-16)
+    } else if (src.includes('diamonds')) {
+        suit = "d"
+        word = src.slice(-18)
+    } else if (src.includes('hearts')) {
+        suit = "h"
+        word = src.slice(-16)
+    } else if (src.includes('clubs')) {
+        suit = "c"
+        word = src.slice(-15)
+    }
 
+    if (word.includes('2')) {
+        val = "2"
+    } else if (word.includes('3')) {
+        val = "3"
+    } else if (word.includes('4')) {
+        val = "4"
+    } else if (word.includes('5')) {
+        val = "5"
+    } else if (word.includes('6')) {
+        val = "6"
+    } else if (word.includes('7')) {
+        val = "7"
+    } else if (word.includes('8')) {
+        val = "8"
+    } else if (word.includes('9')) {
+        val = "9"
+    } else if (word.includes('10')) {
+        val = "10"
+    } else if (word.includes('J')) {
+        val = "J"
+    } else if (word.includes('Q')) {
+        val = "Q"
+    } else if (word.includes('K')) {
+        val = "K"
+    } else if (word.includes('A')) {
+        val = "A"
+    }
+    return `${val}${suit}`
 }
