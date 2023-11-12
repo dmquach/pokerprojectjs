a067d19c84f2b2433a25e85a159c62079d0893de
Dylan <dylanquach@gmail.com>
2023-11-11 20:49:08 -0800

    Add player functions

--- src/scripts/deck.js
+++ src/scripts/deck.js
@@ -1,12 +1,19 @@
 import { Board } from "./board.js"
 
-function Deck () {
+function Deck (p1, p2, p3, p4, p5, p6) {
     this.cardDeck = {
         '2c': 'deck', '3c': 'deck', '4c': 'deck', '5c': 'deck', '6c': 'deck', '7c': 'deck', '8c': 'deck', '9c': 'deck', '10c': 'deck', 'Jc': 'deck', 'Qc': 'deck', 'Kc': 'deck', 'Ac': 'deck',
         '2d': 'deck', '3d': 'deck', '4d': 'deck', '5d': 'deck', '6d': 'deck', '7d': 'deck', '8d': 'deck', '9d': 'deck', '10d': 'deck', 'Jd': 'deck', 'Qd': 'deck', 'Kd': 'deck', 'Ad': 'deck',
         '2s': 'deck', '3s': 'deck', '4s': 'deck', '5s': 'deck', '6s': 'deck', '7s': 'deck', '8s': 'deck', '9s': 'deck', '10s': 'deck', 'Js': 'deck', 'Qs': 'deck', 'Ks': 'deck', 'As': 'deck',
         '2h': 'deck', '3h': 'deck', '4h': 'deck', '5h': 'deck', '6h': 'deck', '7h': 'deck', '8h': 'deck', '9h': 'deck', '10h': 'deck', 'Jh': 'deck', 'Qh': 'deck', 'Kh': 'deck', 'Ah': 'deck'
     }
+    this.p1 = p1
+    this.p2 = p2
+    this.p3 = p3
+    this.p4 = p4
+    this.p5 = p5
+    this.p6 = p6
+
     this.addClickCards()
     this.board = new Board(this)
 }
@@ -19,6 +26,7 @@ Deck.prototype.addClickCards = function () {
             // if in deck and no next part highlighted
             if (this.cardDeck[card.id] === "deck") {
                 this.board.addToBoard(card.id)
+                // add this.playerH
             }
         })
     }
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,10 +1,23 @@
 function Player(active) {
-    this.playerHands = []
+    this.playerHand = []
     this.active = active
 }
 
 Player.prototype.active = function () {
     this.active = !this.active
+}
+
+Player.prototype.addPlayerButton = function () {
+
+}
+
+Player.prototype.removePlayerButton = function () {
+
+}
+
+Player.prototype.addToHand = function (card) {
+    this.playerHand.push(card)
+    console.log(this)
 }
 
 export { Player }
