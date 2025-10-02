11c17143308ae714f8e87b3943be2fd567146602
Dylan <dylanquach@gmail.com>
2025-10-01 23:07:51 -0700

    Update project comments

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,8 @@ const config = {
         use: [
           MiniCssExtractPlugin.loader, // create bundled css file
           {
-            loader: 'css-loader', // resolves @import statements
-            options: { url: false } // don't resolve url() statements
+            loader: 'css-loader',
+            options: { url: false } 
           },
           'sass-loader', // compiles sass to css
         ]
